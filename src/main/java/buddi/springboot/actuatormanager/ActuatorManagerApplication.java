354c0a491413dc3f7fe85f8e91df7b1f23cd9a5d
buddinipun <buddi@Buddhikas-MacBook-Air.local>
2022-06-10 22:51:30 +0530

    initial commit on project setup

---
+++ src/main/java/buddi/springboot/actuatormanager/ActuatorManagerApplication.java
@@ -0,0 +1,13 @@
+package buddi.springboot.actuatormanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/buddi/springboot/actuatormanager/ActuatorManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package buddi.springboot.actuatormanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
